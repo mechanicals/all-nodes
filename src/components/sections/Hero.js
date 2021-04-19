d46273656bcd527769c5ce6ba3312d440ebbd133
shubham sharma <shubham7@live.com>
2021-04-19 19:03:40 +0800

    small changes

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -35,7 +35,7 @@ const Hero = ({
   const closeModal = (e) => {
     e.preventDefault();
     setVideomodalactive(false);
-  }   
+  }
 
   const outerClasses = classNames(
     'hero section center-content',
@@ -61,11 +61,11 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              super<span className="text-color-primary">Nodes</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                We are the leading staking service provider for blockchain projects. Join our community to help secure networks and earn rewards. We know staking.
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
